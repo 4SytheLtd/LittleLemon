9942d5f7e4876214b84c3e7de0a63b201aeb26fb
4SytheLtd <me@s4sythe.com>
2026-02-02 19:48:35 -0600

    models, mysql
    added models.. i suck at github

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.IntegerField()
+    no_of_guests =models.IntegerField()
+    booking_date = models.DateTimeField()
+
+class Booking(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
